9686b1c449232bcc243514e595dddef479b76239
Daniel <danieldcc.colares@gmail.com>
2024-01-20 12:03:38 -0300

    feat: initial explore page

---
+++ src/pages/explore/index.tsx
@@ -0,0 +1,10 @@
+import { ProfileContainer } from '@/styles/pages/profile'
+import { SidebarUserTemplate } from '@/template/SideBarUserTemplate'
+
+export default function Explore() {
+  return (
+    <SidebarUserTemplate>
+      <ProfileContainer> Explore </ProfileContainer>
+    </SidebarUserTemplate>
+  )
+}
